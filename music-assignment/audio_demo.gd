1e1dc5d26e852fa136c0fbd7a6a955b647fd8d37
dylan <C24486954@myTUDublin.ie>
2024-12-05 18:48:27 +0000

    big update
    many changes

--- music-assignment/audio_demo.gd
+++ music-assignment/audio_demo.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _on_button_button_down() -> void:
 	if $AudioStreamPlayer2D.playing:
-		$Button.text = "Play"
+		$Button.text = "Play Song 3"
 		$AudioStreamPlayer2D.stop()
 		
 	else:
@@ -15,9 +15,11 @@ func _on_h_slider_value_changed(value: float) -> void:
 	$AudioStreamPlayer2D.pitch_scale = value
 	pass
 	
+
+	
 func _on_button_2_button_down() -> void:
 	if $AudioStreamPlayer2D2.playing:
-		$Button2.text = "Play"
+		$Button2.text = "Play Song 1"
 		$AudioStreamPlayer2D2.stop()
 		
 	else:
@@ -28,3 +30,50 @@ func _on_button_2_button_down() -> void:
 func _on_h_slider_2_value_changed(value: float) -> void:
 	$AudioStreamPlayer2D2.pitch_scale = value
 	pass
+	
+func _on_button_3_button_down() -> void:
+	if $AudioStreamPlayer2D3.playing:
+		$Button3.text = "Play Song 2"
+		$AudioStreamPlayer2D3.stop()
+		
+	else:
+		$Button3.text = "Stop"
+		$AudioStreamPlayer2D3.play()	
+		pass # Replace with function body.
+	
+func _on_h_slider_3_value_changed(value: float) -> void:
+	$AudioStreamPlayer2D3.pitch_scale = value
+	pass
+
+
+func _on_h_slider_4_value_changed(value: float) -> void:
+	$AudioStreamPlayer2D3.volume_db = value
+	pass # Replace with function body.
+	
+func _on_h_slider_5_value_changed(value: float) -> void:
+	$AudioStreamPlayer2D2.volume_db = value
+	pass # Replace with function body.
+	
+func _on_h_slider_6_value_changed(value: float) -> void:
+	$AudioStreamPlayer2D.volume_db = value
+	pass # Replace with function body.
+	
+func _on_button_4_button_down() -> void:
+	if $AudioStreamPlayer2D4.playing:
+		$Button4.text = "Play Song 2"
+		$AudioStreamPlayer2D4.stop()
+		
+	else:
+		$Button4.text = "Stop"
+		$AudioStreamPlayer2D4.play()	
+		pass # Replace with function body.
+	
+func _on_h_slider_7_value_changed(value: float) -> void:
+	$AudioStreamPlayer2D4.pitch_scale = value
+	pass
+
+
+func _on_h_slider_8_value_changed(value: float) -> void:
+	$AudioStreamPlayer2D4.volume_db = value
+	pass # Replace with function body.
+	
